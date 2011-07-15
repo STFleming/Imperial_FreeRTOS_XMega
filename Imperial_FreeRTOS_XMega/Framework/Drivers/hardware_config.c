8b80e5690d2584ac93927b6849c18720bd312fe8
Shane Fleming <shaneflem@gmail.com>
2011-07-15 16:42:10 +0100

    Added Hardware configuration files

---
+++ Imperial_FreeRTOS_XMega/Framework/Drivers/hardware_config.c
@@ -0,0 +1,13 @@
+/*
+ * hardware_config.c
+ *
+ * Created: 15/07/2011 16:38:05
+ *  Author: sf306
+ */ 
+
+#include "hardware_config.h"
+
+void prvSetupHardware(void)
+{
+	
+}
---
+++ Imperial_FreeRTOS_XMega/Framework/Drivers/hardware_config.h
@@ -0,0 +1,16 @@
+/*
+ * hardware_config.h
+ *
+ * Created: 15/07/2011 16:38:19
+ *  Author: sf306
+ */ 
+
+
+#ifndef HARDWARE_CONFIG_H_
+#define HARDWARE_CONFIG_H_
+
+void prvSetupHardware(void);
+
+
+
+#endif /* HARDWARE_CONFIG_H_ */
